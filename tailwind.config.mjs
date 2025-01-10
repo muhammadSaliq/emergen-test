756aa91d83cd9020881a479d4da6eea0f1c31435
unknown <salik.rafiq11111@gmail.com>
2025-01-10 15:16:22 +0500

    css add 2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -15,3 +15,4 @@ export default {
   },
   plugins: [],
 };
+ 
